611820c65a5e913fb85c88e2d750fe96fc3e18ba
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-03-14 00:31:14 +0600

    Задание 2: реализована логика шифрования и дешифрования на основании шифра Виженера

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app = express();
 const port = 7000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = "cat";
 
 app.get('/Hello', (req, res) => {
     return res.send('Hello');
-})
+});
+
+app.get(`/encode/${password}`, (req, res) => {
+    return res.send(Vigenere.Cipher(password).crypt(password));
+});
+
+app.get('/decode/:cipher', (req, res) => {
+    return res.send(Vigenere.Decipher(password).crypt(req.params.cipher));
+});
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
